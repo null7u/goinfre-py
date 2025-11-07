899348feca17b53679472089136066bc9fa08858
null7u <7null@proton.me>
2025-11-07 15:49:08 +0100

    feat: Add simple cmd interface

--- install.py
+++ install.py
@@ -1,4 +1,36 @@
 #!/bin/python3
 
+import sys
 
-print("Hello im \033[31mnull7u\033[0m")
+global SUPPR_PACKAGES
+SUPPR_PACKAGES= {
+    "uv"    : "https://github.com/astral-sh/uv",
+    "go"    : "https://github.com/golang/go",
+    "nvim"  : "hthttps://github.com/neovim/neovim/",
+    "bat"   : "hthttps://github.com/sharkdp/bat",
+    "lsd"   : "hthttps://github.com/lsd-rs/lsd",
+}
+
+if len(sys.argv) <= 1:
+    print("You did not pass any commands")
+    exit(0)
+cmd_args = [_.lower() for _ in sys.argv[2:]]
+cmd = sys.argv[1].lower()
+
+
+
+def list_packages()->None:
+    # package name, package website or repo
+    for pk_name, pk_url in SUPPR_PACKAGES.items():
+        print(pk_name, pk_url, sep="\t")
+
+
+match cmd:
+    case "list":
+        list_packages()
+    case "install":
+        print("in the future")
+    case _:
+        print("command does not exists.")
+        exit(1)
+
